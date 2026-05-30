54d08db52caedde5898a0ad1a6aa5761b9cfce38
Walter <josuewalterespejoguarachi@gmail.com>
2026-05-30 18:53:23 -0400

    create task repository

--- app/repository/protocol/__init__.py
+++ app/repository/protocol/__init__.py
@@ -1,7 +1,9 @@
 import task_repository_protocol as task
+import token_repository_protocol as token
 import user_repository_interface as user
 
 __all__ = [
     "user",
     "task",
+    "token",
 ]
