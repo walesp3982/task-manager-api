f35942339b261e688a7819a3cd5064e59a147dc3
Walter <josuewalterespejoguarachi@gmail.com>
2026-05-30 17:31:09 -0400

    create task repository

--- app/repository/protocol/__init__.py
+++ app/repository/protocol/__init__.py
@@ -1,5 +1,7 @@
+import task_repository_protocol as task
 import user_repository_interface as user
 
 __all__ = [
     "user",
+    "task",
 ]
---
+++ app/repository/protocol/task_repository_protocol.py
@@ -0,0 +1,36 @@
+from datetime import date
+from typing import Literal, Protocol
+
+from pydantic import BaseModel, Field
+
+from app.entities import CreateTask, StatusTask, Task
+
+
+class DateFilter:
+    order_by: Literal["newest", "oldest"]
+    as_date: date
+
+
+class FilterTask(BaseModel):
+    user_id: set[int] = set()
+    name: str | None = None
+    date_filter: DateFilter | None = None
+    status: set[StatusTask] = set()
+
+
+class PaginationTask(BaseModel):
+    limit: int = Field(ge=0)
+    offset: int = Field(ge=0)
+
+
+class TaskRepositoryProtocol(Protocol):
+    def create(self, task: CreateTask) -> Task: ...
+    def get_by_id(self, id: int) -> Task | None: ...
+    def get_by_filter(
+        self,
+        filter: FilterTask,
+        pagination: PaginationTask,
+    ) -> list[Task]: ...
+    def get_count_filter(self, filter: FilterTask) -> int: ...
+    def update(self, task: Task) -> Task | None: ...
+    def delete(self, id: int) -> bool: ...
